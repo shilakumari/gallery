66ef279fb2449dbb723e7a028f75e7b5a5fe9b5e
shilakumari <shila1996kumari@gmail.com>
2019-09-24 22:19:06 +0530

    Buyer added

--- src/main/java/com/gallery/HelloController.java
+++ src/main/java/com/gallery/HelloController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gallery.buyer.model.Buyer;
+
 @Controller
 //@RequestMapping("/greet")
 public class HelloController {
--- src/main/java/com/gallery/buyer/model/Buyer.java
+++ src/main/java/com/gallery/buyer/model/Buyer.java
@@ -1,4 +1,4 @@
-package com.gallery;
+package com.gallery.buyer.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
